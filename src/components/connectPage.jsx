28355c7013c4eba41ab966e1ab6ad3b6bf8ecc9b
eeedris <alabialade@gmail.com>
2022-04-24 15:15:45 +0100

    Added connect to wallet functionality

--- src/components/connectPage.jsx
+++ src/components/connectPage.jsx
@@ -1,6 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const ConnectPage = () => {
+function log(stuff) {
+  console.log(stuff);
+}
+
+let appMetadata = {
+  name: "Veot",
+  description: "A decentralised polling platform built on the Hedera Hashgraph",
+  icon: "https://absolute.url/to/icon.png"
+};
+
+const saveData = {
+  topic: "",
+  pairingString: "",
+  privateKey: "",
+  pairedWalletData: null,
+  pairedAccounts: []
+};
+
+const ConnectPage = ({ hashconnect, onAccount }) => {
+  const connect = async () => {
+    //first init and store the private for later
+    let initData = await hashconnect.init(appMetadata);
+    saveData.privateKey = initData.privKey;
+
+    //then connect, storing the new topic for later
+    const state = await hashconnect.connect();
+    saveData.topic = state.topic;
+
+    //generate a pairing string, which you can display and generate a QR code from
+    saveData.pairingString = hashconnect.generatePairingString(
+      state,
+      "testnet",
+      true
+    );
+
+    //find any supported local wallets
+    hashconnect.findLocalWallets();
+
+    hashconnect.foundExtensionEvent.once(walletMetadata => {
+      hashconnect.connectToLocalWallet(saveData.pairingString, walletMetadata);
+      localStorage.setItem("veotData", saveData);
+    });
+  };
+
   return (
     <div className="grid grid-cols-[0_auto] lg:grid-cols-[auto_550px] h-screen w-screen">
       <div className="bg-[url(./images/banner.jpg)] bg-cover"></div>
@@ -13,6 +56,7 @@ const ConnectPage = () => {
         </p>
         <button
           type="button"
+          onClick={connect}
           className="bg-gold hover:bg-dgold font-medium py-5 px-12 rounded-3xl mb-10"
         >
           Connect to HashPack Wallet
